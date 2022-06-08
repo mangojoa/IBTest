0f96f2576f0a4fe0c16f985dd5e8ee4af74ad89c
이두산 <sxfc675@gmail.com>
2022-06-08 20:38:24 +0900

    [22.06.08] 객체의 연결 및 정보 확인하기

--- IBTest/ViewController.swift
+++ IBTest/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    /* [22.06.08]
+     @IBOutlet / @IBAction 의 앞에 생기는 O는
+     보조에디터를 추가할 수 있다.
+     
+     프로퍼티나 메소드가 인터페이스 빌더의 객체와 연결되어 있다는 것을 시각적으로 알 수 있다.
+    */
+    @IBOutlet var UniTitle01: UILabel!
+    
+    @IBAction func clickBtn01(_ sender: Any) {
+        
+    }
 }
 
