6cd587817a879bf0947fb79599d859d33c189be0
이두산 <sxfc675@gmail.com>
2022-06-08 20:12:43 +0900

    [22.06.08] init commit

---
+++ IBTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBTest
+//
+//  Created by 이두산 on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
